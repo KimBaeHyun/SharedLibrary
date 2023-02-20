8dd707139f9cc53d57f43cda7a18096f601be92f
김현배 <bae0231@gmail.com>
2023-02-20 16:26:34 +0900

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,1 +1,23 @@
 
+
+def mvn() {
+  properties([parameters([string(defaultValue: 'test', description: 'test or verify', name: 'testType', trim: false)])])
+
+  node {
+      stage('git') {
+          git branch: 'failed', url: 'https://github.com/hyunil-shin/java-maven-junit-helloworld.git'
+      }
+
+      stage('build') {
+          withEnv(["PATH+MAVEN=${tool 'mvn-3.9.0'}/bin", "JAVA_HOME=${tool 'openjdk10'}"]) {
+              sh 'mvn --version'           
+              sh "mvn clean ${params.testType} -Dmaven.test.failure.ignore=true"
+          }
+      }
+
+      stage('report') {
+          junit 'target/surefire-reports/*.xml'
+          jacoco execPattern: 'target/**.exec'
+      }
+  }
+}
